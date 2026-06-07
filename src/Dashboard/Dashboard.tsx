2168022051b1d81ac09de3204d496bf5d1f7122e
Mojtaba Alizadeh <moj.dazalih@gmail.com>
2026-06-07 13:49:56 +0330

    make header component

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,15 +1,8 @@
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import MainSidebar from "./Sidebar/MainSidebar"
-import { useEffect, useState } from "react"
-import { TypingAnimation } from "@/components/ui/typing-animation"
 import Home from "./Home/Home"
-import { Route, Routes, useLocation } from "react-router"
+import { Route, Routes } from "react-router"
 import Categories from "./Categories/Categories"
-import { usePathTitle } from "@/hooks/usePathTitle"
 import Products from "./Products/Products"
 import Brands from "./Brands/Brands"
 import Guarantees from "./Guarantees/Guarantees"
@@ -23,59 +16,14 @@ import Access from "./Access/Access"
 import Comments from "./Comments/Comments"
 import Questions from "./Questions/Questions"
 import Users from "./Users/Users"
+import Header from "./Header/Header"
 
 const Dashboard = () => {
-  const [clock, setClock] = useState<string>()
-  const [weekDay, setWeekDay] = useState<string>()
-  const [state, dispatch] = usePathTitle()
-  const pathTitle = useLocation()
-
-  useEffect(() => {
-    console.log(pathTitle.pathname)
-    dispatch({ type: pathTitle.pathname })
-  }, [pathTitle])
-
-  useEffect(() => {
-    setInterval(() => {
-      setClock(
-        new Intl.DateTimeFormat("fa-IR", {
-          year: "numeric",
-          month: "long",
-          day: "2-digit",
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-          hour12: false,
-        }).format(new Date())
-      )
-    }, 1000)
-    setWeekDay(
-      new Intl.DateTimeFormat("fa-IR", {
-        weekday: "long",
-      }).format(new Date())
-    )
-  }, [])
-
   return (
     <SidebarProvider className="bg-muted p-2">
       <MainSidebar />
       <SidebarInset className="flex w-full flex-col gap-4 rounded-xl bg-mauve-800 p-3 shadow-2xl">
-        <header className="flex items-center justify-between border-b-2 pb-2">
-          <SidebarTrigger className="cursor-pointer" />
-          <h1 className="text-5xl font-black">
-            <TypingAnimation blinkCursor={true} pauseDelay={2000}>
-              {state}
-            </TypingAnimation>
-          </h1>
-          <div className="w-55 text-center">
-            <h1 className="text-3xl font-bold text-blue-300">{weekDay}</h1>
-            <h1 className="text-1xl font-semibold text-blue-200">
-              {clock || (
-                <div className="mx-auto h-4 w-50 animate-pulse rounded-2xl bg-mauve-700"></div>
-              )}
-            </h1>
-          </div>
-        </header>
+        <Header />
         <main className="mx-5">
           <Routes>
             <Route path="/" element={<Home />} />
---
+++ src/Dashboard/Header/Header.tsx
@@ -0,0 +1,59 @@
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import { TypingAnimation } from "@/components/ui/typing-animation"
+import { usePathTitle } from "@/hooks/usePathTitle"
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router"
+
+const Header = () => {
+  const [clock, setClock] = useState<string>()
+  const [weekDay, setWeekDay] = useState<string>()
+  const [state, dispatch] = usePathTitle()
+  const pathTitle = useLocation()
+
+  useEffect(() => {
+    console.log(pathTitle.pathname)
+    dispatch({ type: pathTitle.pathname })
+  }, [pathTitle])
+
+  useEffect(() => {
+    setInterval(() => {
+      setClock(
+        new Intl.DateTimeFormat("fa-IR", {
+          year: "numeric",
+          month: "long",
+          day: "2-digit",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+          hour12: false,
+        }).format(new Date())
+      )
+    }, 1000)
+    setWeekDay(
+      new Intl.DateTimeFormat("fa-IR", {
+        weekday: "long",
+      }).format(new Date())
+    )
+  }, [])
+
+  return (
+    <header className="flex items-center justify-between border-b-2 pb-2">
+      <SidebarTrigger className="cursor-pointer" />
+      <h1 className="text-5xl font-black">
+        <TypingAnimation blinkCursor={true} pauseDelay={2000}>
+          {state}
+        </TypingAnimation>
+      </h1>
+      <div className="w-55 text-center">
+        <h1 className="text-3xl font-bold text-blue-300">{weekDay}</h1>
+        <h1 className="text-1xl font-semibold text-blue-200">
+          {clock || (
+            <div className="mx-auto h-4 w-50 animate-pulse rounded-2xl bg-mauve-700"></div>
+          )}
+        </h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
